5d5fa53fde0e5ada000642116535c13bd7ee9f5d
waltl <waltl@10.140.236.137>
2020-09-28 11:53:44 +1300

    Added start of cell class

---
+++ GameOfLife/src/Cell.java
@@ -0,0 +1,29 @@
+
+public class Cell {
+	Cell left;
+	Cell right;
+	Cell up;
+	Cell down;
+	int x;
+	int y;
+	Boolean alive;
+
+	public Cell() {
+		x = 0;
+		y = 0;
+		alive = true;
+	}
+	
+	public Cell(Cell origin, int x, int y) {
+		this.x = x;
+		this.y = y;
+		if (origin.x == this.x) {
+			if (this.y - origin.y == 1) {
+				up = origin;
+			} else if (this.y - origin.y == -1) {
+				down = origin;
+			}
+		}
+	}
+
+}
