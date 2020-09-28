f88e616f8aad0bbcde4453d15f303c2d1a8f456b
waltl <waltl@10.140.236.137>
2020-09-28 14:12:05 +1300

    added design plan

--- GameOfLife/src/Cell.java
+++ GameOfLife/src/Cell.java
@@ -17,18 +17,10 @@ public class Cell {
 	public Cell(Cell origin, int x, int y) {
 		this.x = x;
 		this.y = y;
-		if (origin.x == this.x) {
-			if (this.y - origin.y == 1) {
-				up = origin;
-			} else if (this.y - origin.y == -1) {
-				down = origin;
-			}
-		}
-		else if (origin.y == this.y) {
-			if (this.x - origin.x == 1) {
-				left = origin;
-			} else if (this.x - origin.x == -1) {
-				right = origin;
+		if (origin.y < this.y) {
+			Cell node = origin;
+			while (node != null && node.y < this.y) {
+				node = node.down;
 			}
 		}
 	}
