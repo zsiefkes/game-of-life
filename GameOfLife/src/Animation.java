5e32bfec23bd1682e6f19718753f64504300b748
bunchmatt <bunchmatt@gs161d.AD.ECS.VUW.AC.NZ>
2020-09-30 13:08:06 +1300

    Keyboard functionality + alert box class

--- GameOfLife/src/Animation.java
+++ GameOfLife/src/Animation.java
@@ -21,8 +21,20 @@ public class Animation extends Application {
 	public static final int WINDOW_WIDTH = 700;
 	// screen height
 	public static final int WINDOW_HEIGHT = 400;
+	// rows and columns input
+	int rowsInput = (int) (WINDOW_HEIGHT / SIZE);
+	int colsInput = (int) (WINDOW_WIDTH / SIZE);
 	// game animation is running boolean status
 	boolean gameOn;
+	boolean normalSpeed;
+	// info text for the game
+	String helpText = "SPACE: pause game\n"
+			+ "UP: toggle 2.0 speed\n"
+			+ "DOWN: toggle 0.5 speed\n"
+			+ "R: reset world\n"
+			+ "H: help box\n"
+			+ "L: load new seed\n"
+			+ "C: input custom seed";
 
 	private void drawGrid(Group group) {
 		for (int i = 0; i < WINDOW_HEIGHT; i += SIZE) {
@@ -43,7 +55,7 @@ public class Animation extends Application {
 		for (int i = 0; i < world.getCoordinatesList().size(); i++) {
 			int x = world.getCoordinatesList().get(i).getX();
 			int y = world.getCoordinatesList().get(i).getY();
-			Rectangle rect = new Rectangle(x*SIZE, y*SIZE, SIZE, SIZE);
+			Rectangle rect = new Rectangle(x * SIZE, y * SIZE, SIZE, SIZE);
 			rect.setFill(Color.color(Math.random(), Math.random(), Math.random())); // random color, change this if
 																					// necessary
 			group.getChildren().add(rect);
@@ -64,10 +76,22 @@ public class Animation extends Application {
 		drawWorld(world, group);
 	}
 
+	private void resetWorld(World world) {
+		world.setRows(rowsInput);
+		world.setCols(colsInput);
+		/*
+		 * TODO: future method in World, take for loop that initializes the world's list
+		 * and put it in its own method:
+		 */
+		// world.initializeValuesList
+		world.initializeCoordinatesList();
+		world.readSeed();
+	}
+
 	@Override
 	public void start(Stage window) throws Exception {
 		// TODO:
-		World world = new World((int) (WINDOW_HEIGHT / SIZE), (int) (WINDOW_WIDTH / SIZE));
+		World world = new World(rowsInput, colsInput);
 		Group root = new Group();
 		drawWorld(world, root);
 		KeyFrame frame = new KeyFrame(Duration.millis(200), new EventHandler<ActionEvent>() {
@@ -81,8 +105,9 @@ public class Animation extends Application {
 
 		Timeline t = new Timeline(frame);
 		t.setCycleCount(javafx.animation.Animation.INDEFINITE);
-		gameOn = true;
 		t.play();
+		gameOn = true;
+		normalSpeed = true;
 
 		Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
 		// adding keyboard functionality
@@ -96,9 +121,32 @@ public class Animation extends Application {
 					t.play();
 					gameOn = true;
 				}
+			}
+			// double speed
+			else if (key.getCode() == KeyCode.UP)
+				if (normalSpeed) {
+					t.setRate(2);
+					normalSpeed = false;
+				} else {
+					t.setRate(1);
+					normalSpeed = true;
+				}
+			// half speed
+			else if (key.getCode() == KeyCode.DOWN)
+				if (normalSpeed) {
+					t.setRate(0.5);
+					normalSpeed = false;
+				} else {
+					t.setRate(1);
+					normalSpeed = true;
+				}
 			// reset world
-			} else if (key.getCode() == KeyCode.R) {
-				
+			else if (key.getCode() == KeyCode.R) {
+				resetWorld(world);
+			}
+			// help box of keyboard codes
+			else if (key.getCode() == KeyCode.H) {
+				AlertBox.display("Information", helpText);
 			}
 		});
 
