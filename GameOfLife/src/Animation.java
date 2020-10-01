63fb53381a6b48f26115057a98b82002a23d5cac
waltl <waltl@10.140.152.177>
2020-10-01 13:20:28 +1300

    Doubled the pufferfish

--- GameOfLife/src/Animation.java
+++ GameOfLife/src/Animation.java
@@ -18,9 +18,9 @@ public class Animation extends Application {
 	// size
 	public static final int SIZE = 20;
 	// screen width
-	public static final int WINDOW_WIDTH = 600;
+	public static final int WINDOW_WIDTH = 1000;
 	// screen height
-	public static final int WINDOW_HEIGHT = 500;
+	public static final int WINDOW_HEIGHT = 600;
 	// rows and columns input
 	int rowsInput = (int) (WINDOW_HEIGHT / SIZE);
 	int colsInput = (int) (WINDOW_WIDTH / SIZE);
